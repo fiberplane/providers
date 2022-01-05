561158ea8ec22732fc5c65fc11068ce24c216df5
Simon Rasmussen <zetlon@gmail.com>
2022-01-05 10:33:55 +0100

    Implement LOKI provider (#26)

---
+++ providers/loki/README.md
@@ -0,0 +1,5 @@
+# LOKI provider 
+
+## Development
+A local loki instance can be run with the `docker-compose.yml` file by running `docker-compose up -d` in the current directory. 
+To shut it down again run `docker-compose down` from the same directory.
---
+++ providers/loki/src/lib.rs
@@ -0,0 +1,204 @@
+use fp_provider::{
+    fp_export_impl, make_http_request, Config, Error, HttpRequest, HttpRequestMethod, LogRecord,
+    ProviderRequest, ProviderResponse, QueryLogs, Timestamp,
+};
+use serde::Deserialize;
+use std::{collections::HashMap, str::FromStr};
+use url::Url;
+
+#[fp_export_impl(fp_provider)]
+async fn invoke(request: ProviderRequest, config: Config) -> ProviderResponse {
+    match request {
+        // TODO implement AutoSuggest
+        ProviderRequest::Logs(query) => fetch_logs(query, config)
+            .await
+            .map(|log_records| ProviderResponse::LogRecords { log_records })
+            .unwrap_or_else(|error| ProviderResponse::Error { error }),
+        _ => ProviderResponse::Error {
+            error: Error::UnsupportedRequest,
+        },
+    }
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+struct QueryResponse {
+    status: String,
+    data: QueryData,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[serde(tag = "resultType", content = "result", rename_all = "camelCase")]
+enum QueryData {
+    Streams(Vec<Data>),
+    Scalar {},
+    Vector {},
+    Matrix(Vec<Data>),
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+struct Data {
+    #[serde(alias = "stream", alias = "metric")]
+    labels: HashMap<String, String>,
+    values: Vec<(String, String)>,
+}
+
+async fn fetch_logs(query: QueryLogs, config: Config) -> Result<Vec<LogRecord>, Error> {
+    let url = config.url.ok_or_else(|| Error::Config {
+        message: "URL is required".to_string(),
+    })?;
+    let mut url = Url::parse(&url).map_err(|e| Error::Config {
+        message: format!("Invalid LOKI URL: {:?}", e),
+    })?;
+
+    {
+        let mut path_segments = url.path_segments_mut().map_err(|_| Error::Config {
+            message: "Invalid LOKI URL".to_string(),
+        })?;
+        path_segments
+            .push("loki")
+            .push("api")
+            .push("v1")
+            .push("query_range");
+    }
+
+    let qstr: String =
+        url::form_urlencoded::Serializer::new(String::with_capacity(query.query.capacity()))
+            .append_pair("query", &query.query)
+            .append_pair(
+                "limit",
+                &query.limit.map_or("".to_owned(), |l| l.to_string()),
+            )
+            .append_pair("start", &((query.time_range.from) as u64).to_string())
+            .append_pair("end", &((query.time_range.to) as u64).to_string())
+            .finish();
+    url.set_query(Some(&qstr));
+
+    let request = HttpRequest {
+        body: None,
+        headers: None,
+        method: HttpRequestMethod::Post,
+        url: url.to_string(),
+    };
+
+    // Parse response
+    let response = make_http_request(request)
+        .await
+        .map_err(|error| match &error {
+            fp_provider::HttpRequestError::ServerError {
+                status_code,
+                response,
+            } if *status_code == 400 => Error::Other {
+                message: format!("Query error: {}", String::from_utf8_lossy(response)),
+            },
+            _ => Error::Http { error },
+        })?
+        .body;
+
+    let response: QueryResponse = serde_json::from_slice(&response).map_err(|e| Error::Data {
+        message: format!("Error parsing LOKI response: {:?}", e),
+    })?;
+
+    if response.status != "success" {
+        return Err(Error::Data {
+            message: format!("Query didn't succeed, returned: \"{}\"", response.status),
+        });
+    }
+
+    let data = match response.data {
+        QueryData::Streams(d) => d,
+        QueryData::Matrix(d) => d,
+        _ => {
+            return Err(Error::Data {
+                message: format!("Query didn't return a stream or matrix"),
+            })
+        }
+    };
+
+    let logs = data
+        .iter()
+        .flat_map(data_mapper)
+        .collect::<Result<Vec<LogRecord>, _>>()
+        .map_err(|e| Error::Data {
+            message: format!("Failed to parse data, got error: {:?}", e),
+        })?;
+
+    Ok(logs)
+}
+
+fn data_mapper<'a>(
+    d: &'a Data,
+) -> impl Iterator<Item = Result<LogRecord, <Timestamp as FromStr>::Err>> + 'a {
+    let att = &d.labels;
+    d.values.iter().map(move |(ts, v)| {
+        let timestamp = Timestamp::from_str(ts)?;
+        Ok(LogRecord {
+            timestamp,
+            body: v.clone(),
+            attributes: att.clone(),
+            span_id: None,
+            trace_id: None,
+            resource: HashMap::default(),
+        })
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{Data, QueryData, QueryResponse};
+    use serde::Deserialize;
+    use serde_json::Deserializer;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_deserialization() {
+        let data = r#"{
+            "status": "success",
+            "data": {
+              "resultType": "streams",
+              "result": [
+                {
+                  "stream": {
+                    "filename": "/var/log/myproject.log",
+                    "job": "varlogs",
+                    "level": "info"
+                  },
+                  "values": [
+                    [
+                      "1569266497240578000",
+                      "foo"
+                    ],
+                    [
+                      "1569266492548155000",
+                      "bar"
+                    ]
+                  ]
+                }
+              ],
+              "stats": {
+              }
+            }
+          }"#;
+
+        let value = QueryResponse::deserialize(&mut Deserializer::from_str(data)).unwrap();
+
+        assert_eq!(
+            value,
+            QueryResponse {
+                data: QueryData::Streams(vec![Data {
+                    labels: HashMap::from([
+                        ("filename".to_owned(), "/var/log/myproject.log".to_owned()),
+                        ("job".to_owned(), "varlogs".to_owned()),
+                        ("level".to_owned(), "info".to_owned()),
+                    ]),
+                    values: vec![
+                        ("1569266497240578000".to_owned(), "foo".to_owned()),
+                        ("1569266492548155000".to_owned(), "bar".to_owned()),
+                    ],
+                }]),
+                status: "success".to_owned(),
+            },
+        )
+    }
+}
