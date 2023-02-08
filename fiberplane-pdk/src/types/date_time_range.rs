65149b4bfa8a2a36f04511c5ddf65121177f6201
Gerry Agbobada <gerry@fiberplane.com>
2023-02-08 11:28:34 +0100

    Implement a custom Deserialize for DateTimeRange

--- fiberplane-pdk/src/types/date_time_range.rs
+++ fiberplane-pdk/src/types/date_time_range.rs
@@ -1,12 +1,57 @@
-use serde::{Deserialize, Serialize};
+mod deserialize;
+use serde::Serialize;
 use time::OffsetDateTime;
 
 /// Type to use for query data fields of type "date_time_range".
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
 pub struct DateTimeRange {
     /// Start time of the range, inclusive.
     pub from: OffsetDateTime,
 
     /// End time of the range, exclusive.
     pub to: OffsetDateTime,
+}
+
+#[cfg(test)]
+mod tests {
+    use serde::Deserialize;
+    use time::macros::datetime;
+
+    use super::*;
+    #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+    struct TargetType {
+        range: DateTimeRange,
+    }
+
+    #[test]
+    fn deserialize_from_url() {
+        // URL encoding of "2023-02-08T09:16:27.794Z 2023-02-08T09:31:27.794Z"
+        let input = "range=2023-02-08T09%3A16%3A27.794Z%202023-02-08T09%3A31%3A27.794Z";
+        let target: TargetType = serde_html_form::from_str(input).unwrap();
+        assert_eq!(
+            target,
+            TargetType {
+                range: DateTimeRange {
+                    from: datetime!(2023-02-08 09:16:27.794 +00:00),
+                    to: datetime!(2023-02-08 09:31:27.794 +00:00)
+                }
+            }
+        )
+    }
+
+    #[test]
+    fn deserialize_from_bytes() {
+        // URL encoding of "2023-02-08T09:16:27.794Z 2023-02-08T09:31:27.794Z"
+        let input = b"range=2023-02-08T09%3A16%3A27.794Z%202023-02-08T09%3A31%3A27.794Z";
+        let target: TargetType = serde_html_form::from_bytes(input).unwrap();
+        assert_eq!(
+            target,
+            TargetType {
+                range: DateTimeRange {
+                    from: datetime!(2023-02-08 09:16:27.794 +00:00),
+                    to: datetime!(2023-02-08 09:31:27.794 +00:00)
+                }
+            }
+        )
+    }
 }
---
+++ fiberplane-pdk/src/types/date_time_range/deserialize.rs
@@ -0,0 +1,41 @@
+use super::DateTimeRange;
+use serde::de::{self, Visitor};
+use serde::Deserialize;
+use std::fmt;
+use time::format_description::well_known::Rfc3339;
+use time::OffsetDateTime;
+
+impl<'de> Deserialize<'de> for DateTimeRange {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(DateTimeRangeVisitor)
+    }
+}
+
+struct DateTimeRangeVisitor;
+
+impl<'de> Visitor<'de> for DateTimeRangeVisitor {
+    type Value = DateTimeRange;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter
+            .write_str("a string containing 2 timestamps in RFC3339 format separated by a space")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if let Some((from_ts, to_ts)) = value.split_once(" ") {
+            let from = OffsetDateTime::parse(from_ts, &Rfc3339)
+                .map_err(|e| E::custom(format!("could not parse the 'from' timestamp: {e}")))?;
+            let to = OffsetDateTime::parse(to_ts, &Rfc3339)
+                .map_err(|e| E::custom(format!("could not parse the 'to' timestamp: {e}")))?;
+            Ok(DateTimeRange { from, to })
+        } else {
+            Err(E::custom("wrong format. The correct format is '{from_rfc_3339_timestamp} {to_rfc_3339_timestamp}'. There is a single space between the timestamps."))
+        }
+    }
+}
