f978e12f4724a14abb2b8b5458b232e3c952852c
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2023-02-10 10:00:09 +0100

    Small touches to PDK/sample provider to make sure sideloading the provider works "as is" (#7)
    * Stop using select field for the sample provider
    
    Studio does not support Select Fields yet
    
    * Implement a custom Deserialize for DateTimeRange
    * Add custom Serialize for DateTimeRange
    * Fix format of cells
    * Remove log message, better display of tags in sample provider

--- fiberplane-pdk/src/parse_query.rs
+++ fiberplane-pdk/src/parse_query.rs
@@ -18,3 +18,37 @@ pub fn parse_query<T: DeserializeOwned>(query_data: Blob) -> Result<T> {
         message: err.to_string(),
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::DateTimeRange;
+    use serde::Deserialize;
+    use time::macros::datetime;
+
+    #[derive(Deserialize, PartialEq, Eq, Debug)]
+    struct ShowcaseQueryData {
+        pub query: String,
+        pub time_range: DateTimeRange,
+        #[serde(default)]
+        pub live: bool,
+        pub tags: String,
+    }
+
+    #[test]
+    fn parse_sample_provider_query() {
+        let input = b"query=I%27m+a+pretty+query%21%21&tags=Test&time_range=2023-02-08T09%3A16%3A27.794Z+2023-02-08T09%3A31%3A27.794Z";
+        let actual: ShowcaseQueryData = serde_html_form::from_bytes(input).unwrap();
+        assert_eq!(
+            actual,
+            ShowcaseQueryData {
+                query: "I'm a pretty query!!".to_string(),
+                time_range: DateTimeRange {
+                    from: datetime!(2023-02-08 09:16:27.794 +00:00),
+                    to: datetime!(2023-02-08 09:31:27.794 +00:00)
+                },
+                live: false,
+                tags: "Test".to_string()
+            }
+        );
+    }
+}
