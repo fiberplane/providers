bfd0496f0bf80713b783cd23cba0b36b02635ff1
Arend van Beelen <arendjr@gmail.com>
2023-03-31 11:05:21 +0200

    We don't do alphas anymore

--- xtask/src/commands/publish.rs
+++ xtask/src/commands/publish.rs
@@ -4,17 +4,13 @@ use clap::Parser;
 use console::style;
 use duct::cmd;
 use fiberplane_ci::utils::*;
-use fiberplane_ci::{commands::versions::*, TaskResult};
+use fiberplane_ci::TaskResult;
 
 /// Publishable crates in order of publication.
 const CRATE_DIRS: &[&str] = &["fiberplane-pdk-macros", "fiberplane-pdk"];
 
 #[derive(Parser)]
 pub struct PublishArgs {
-    /// Publish all crates that can be published, instead of only changed ones.
-    #[clap(long)]
-    pub all: bool,
-
     /// Do not actually publish the release(s).
     ///
     /// Note that publication will fail if you try to dry-run publication for
@@ -23,82 +19,9 @@ pub struct PublishArgs {
     /// themselves.
     #[clap(long)]
     pub dry_run: bool,
-
-    #[clap(subcommand)]
-    sub_command: PublishCommand,
-}
-
-#[derive(Parser)]
-pub enum PublishCommand {
-    /// Publishes all the crates that changed in the last commit under a new
-    /// alpha version.
-    Alphas,
-
-    /// Publishes all the crates with an unpublished beta version.
-    Betas,
 }
 
 pub async fn handle_publish_command(args: &PublishArgs) -> TaskResult {
-    match &args.sub_command {
-        PublishCommand::Alphas => handle_publish_alphas(args).await,
-        PublishCommand::Betas => handle_publish_betas(args).await,
-    }
-}
-
-async fn handle_publish_alphas(args: &PublishArgs) -> TaskResult {
-    if args.all {
-        eprintln!("{NOTE}Will publish alpha versions for all publishable crates.");
-    } else {
-        let commits = get_commits(".")?;
-        let previous_commit = commits
-            .get(1)
-            .context("Could not determine previous commit")?;
-
-        eprintln!("{NOTE}Will publish alpha versions if crates changed since {previous_commit}.");
-
-        if !CRATE_DIRS
-            .iter()
-            .any(|crate_dir| did_change(crate_dir, previous_commit).unwrap_or_default())
-        {
-            eprintln!("{SUCCESS}No crates need publishing.");
-            return Ok(());
-        }
-    }
-
-    eprintln!("{WORKING}Updating necessary Cargo files...");
-
-    let workspace_cargo_toml = TomlNode::from_file("Cargo.toml")?;
-    let mut workspace_version = workspace_cargo_toml
-        .get_string("workspace.package.version")
-        .context("Cannot determine workspace version")?;
-
-    workspace_version =
-        determine_next_workspace_alpha(CRATES_IO_INDEX_URL, &workspace_version, &CRATE_DIRS)
-            .await?;
-
-    set_version(
-        "Cargo.toml",
-        &SetVersionArgs {
-            crate_name: None,
-            version: workspace_version.clone(),
-        },
-    )?;
-
-    eprintln!(
-        " - Workspace => {version}.",
-        version = style(&workspace_version).bold()
-    );
-
-    eprintln!("{CHECK}Cargo file patched. Starting publication...");
-
-    publish_crates(args)?;
-
-    eprintln!("{SUCCESS}All crates published.");
-
-    Ok(())
-}
-
-async fn handle_publish_betas(args: &PublishArgs) -> TaskResult {
     eprintln!("{WORKING}Detecting crates that need publication...");
 
     let workspace_version = TomlNode::from_file("Cargo.toml")?
--- xtask/src/constants.rs
+++ xtask/src/constants.rs
@@ -3,7 +3,6 @@ use console::Emoji;
 pub(crate) static BUILD: Emoji<'_, '_> = Emoji("🔨 ", "");
 pub(crate) static CHECK: Emoji<'_, '_> = Emoji("✅ ", "");
 pub(crate) static ERROR: Emoji<'_, '_> = Emoji("🤒 ", "");
-pub(crate) static NOTE: Emoji<'_, '_> = Emoji("📝 ", "");
 pub(crate) static OPTIMIZE: Emoji<'_, '_> = Emoji("📦 ", "");
 pub(crate) static SUCCESS: Emoji<'_, '_> = Emoji("✨ ", "");
 pub(crate) static WARN: Emoji<'_, '_> = Emoji("⚠️ ", "");
